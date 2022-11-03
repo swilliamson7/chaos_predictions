26899bc541a3770b87b8591bd609eab179c8b64a
Sarah Williamson <swilliamson@utexas.edu>
2022-11-03 14:51:56 -0500

    updating ad functions

--- Lorenz/lorenz_model.jl
+++ Lorenz/lorenz_model.jl
@@ -1,18 +1,10 @@
-# parameter struct 
-
-mutable struct lorenz_params 
-    rho::Float64
-    sigma::Float64
-    beta::Float64
-end 
-
 # building functions for the Lorenz model 
 
 # run one step of the nonlinear model
 # inputs: out - a shadow output, we copy to this the state after taking one timestep 
 #         dt - timestep
 #         x - the state at the current timestep
-#         params - structure containing rho, sigma, beta 
+#         rho, sigma, beta - the Lorenz parameters
 function forward_step(out, dt, x, rho, sigma, beta) 
 
     dx = zeros(3)
@@ -21,14 +13,16 @@ function forward_step(out, dt, x, rho, sigma, beta)
     dx[2] = x[1] * (rho - x[3]) - x[2]
     dx[3] = x[1] * x[2] - beta * x[3] 
 
-    new = x + dt .* dx 
+    new = zeros(3)
+    new[1] = x[1] + dt * dx[1]
+    new[2] = x[2] + dt * dx[2] 
+    new[3] = x[3] + dt * dx[3]
+
     copyto!(out, new)
 
 end
 
-# Creates data that we can use to train our neural network, 
-# it returns one specific trajectory with a given q and l and returns 
-# what that trajectory was at every step. 
+# Creates a data point (timeseries) that we can use to train our neural network 
 # Input: 
 #           T - total steps to take 
 #           dt - timestep 
@@ -44,13 +38,15 @@ function generate_trajectory(T, dt, state0, params)
     for j = 2:T
         out = zeros(3)
         forward_step(out, dt, all_states[:, j-1], params.rho, params.sigma, params.beta) 
-        all_states[:, j] = out
+        all_states[:, j] = out[:]
     end
 
     return all_states
 
 end
 
+# Same as above function except gives the option to feed individual parameters
+# rather than a structure containing all of them 
 function generate_trajectory(T, dt, state0, rho, sigma, beta)
 
     all_states = zeros(3, T)
@@ -59,7 +55,7 @@ function generate_trajectory(T, dt, state0, rho, sigma, beta)
     for j = 2:T
         out = zeros(3)
         forward_step(out, dt, all_states[:, j-1], rho, sigma, beta) 
-        all_states[:, j] = out
+        all_states[:, j] = out[:]
     end
 
     return all_states
@@ -73,21 +69,23 @@ end
 #       dt - timestep
 #       state_now - state at the current iteration 
 #       adjoint_old - the prior (formally future) adjoint value 
+#       rho, sigma, beta - Lorenz parameters
 # Outputs: 
 #       d_state_now - the new adjoint variable 
-function ad_step(dt, state_now, adjoint_old, params)
+function ad_step(dt, state_now, adjoint_old, rho, sigma, beta)
     
-    adjoint_new = zeros(2) 
-    drho, dsigma, dbeta = autodiff(forward_step, 
-            Duplicated(state_now, adjoint_new),
-            dt, 
-            Duplicated(zeros(2), adjoint_old), 
-            Active(params.rho),
-            Active(params.sigma),
-            Active(params.beta)
-            )
+    adjoint_new = zeros(3) 
+    output_state = zeros(3)
+    autodiff(forward_step, Const,
+            Duplicated(output_state, adjoint_old),
+            Const(dt), 
+            Duplicated(state_now, adjoint_new), 
+            Const(rho),
+            Const(sigma),
+            Const(beta)
+    )
 
-    return drho, dsigma, dbeta, adjoint_new 
+    return adjoint_new 
 
 end
 
@@ -98,28 +96,26 @@ end
 #       data - the data values themselves
 #       dt - timestep
 #       T - final time
-#       f - forcing *function*
-#       g - gravitational acceleration
 #       state0 - initial state of the system 
-#       q0 - initial guess for q, the damping coefficient
-#       l0 - initial guess for l, the pendulum length 
+#       params - a structure containing the parameters in the Lorenz model, 
+#                rho, sigma, and beta 
 # Outputs: 
 #       states - all of the results from the forward run with the 
 #                inputted parameters
 #       adjoint_variables - the adjoint variables (Lagrange multipliers)
 #                that were computed 
-function adjoint(data_steps, data, dt, T, state0, params)
+function adjoint(data_steps, data, dt, T, state0, rho, sigma, beta)
 
-states = generate_trajectory(T, dt, state0, params)
+states = generate_trajectory(T, dt, state0, rho, sigma, beta)
 
 # next we want to run the adjoint problem backward and compute 
 # the adjoint variables 
-adjoint_variables = zeros(2, Int(T/dt))
+adjoint_variables = zeros(3, T)
 
-for j = Int(T/dt)-1:-1:1
+for j = T-1:-1:1
 
     adjoint_old = adjoint_variables[:, j+1]
-    d_state_now = ad_step(dt, states[:,j], adjoint_old, params)
+    d_state_now = ad_step(dt, states[:,j], adjoint_old, rho, sigma, beta)
     adjoint_variables[:,j] .= d_state_now[:]
 
     # this statement checks if we have a data point at the current iteration, and if so adjusts
